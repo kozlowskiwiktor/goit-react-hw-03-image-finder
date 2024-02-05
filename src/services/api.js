63af2fe71adbd79b2c5dbec66f428fa46c51da4e
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2024-02-05 21:26:17 +0100

    fixed api.js
    removed = before q

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ const baseURL = 'https://pixabay.com/api/';
 
 export const fetchMovies = (searchQuery, currentPage) => {
     return fetch(
-        `${baseURL}?=q=${searchQuery}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+        `${baseURL}?q=${searchQuery}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
     ).then(response => {
         if (response.ok) {
             return response.json();
